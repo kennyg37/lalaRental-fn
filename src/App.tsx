f4ba6c5bb47b2933fdefa3362d3c179e34de070d
kennyg37 <k.ganza@alustudent.com>
2025-02-18 21:20:04 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App(){
+  return (
+    <div className='h-screen flex flex-col items-center justify-center space-y-4'>
+      <h1 className='text-4xl font-bold'>Hello this is LalaRent</h1>
+      <p className='text-lg text-gray-500'>The application will lauch soon stay tuned</p>
+    </div>
+  )
+}
+
+export default App
